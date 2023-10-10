04eda45c9c5f2812f444fd1a15e94f111d1936d1
VYSHAKHAK88 <vyshakhak17hp@gmail.com>
2023-10-10 20:57:53 +0530

    netflix design

---
+++ src/component/Row.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import tmdbAxiosInstance from '../tmdbAxiosIntance';
+import './Row.css';
+
+function Row({title,fetchUrl,isPoster}) {
+    const [allMovies,setAllMovies] = useState()
+    const base_url = "https://image.tmdb.org/t/p/original/";
+
+    // console.log(fetchUrl);
+    const fetchData = async ()=>{
+      const {data} = await tmdbAxiosInstance.get(fetchUrl)
+      // console.log(data.results);
+      setAllMovies(data.results)
+
+    }
+     console.log(allMovies);
+    useEffect (()=>{
+     fetchData()},[])
+
+  return (
+    <div className='row'>
+        <h1>{title}</h1>
+        <div className='movies-row'>
+          {
+            allMovies?.map(item=>(
+              <img className={`${isPoster && 'movie-poster'} movie`} src={`${base_url}/${isPoster?item.poster_path:item?.backdrop_path}`} alt="no image" />
+            ))
+          }
+        </div>
+    </div>
+  )
+}
+
+export default Row
